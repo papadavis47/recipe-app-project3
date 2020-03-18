dd2fcd444ee0791d173fb0238b769406c9d5e66e
deanjheikkila <deanjheikkila@gmail.com>
2020-03-18 15:56:23 -0700

    added placeholder pages in client, added auth in app.js and updated Nav to have conditional rendering

--- client/src/wrappers/Nav.js
+++ client/src/wrappers/Nav.js
@@ -2,7 +2,40 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function Nav(props) {
-    // TODO: implement conditional rendering with auth
+    const handleLogout = e => {
+        e.preventDefault()
+        // Remove the token from localstorage
+        localStorage.removeItem('mernToken')
+        // TODO: Update the state of the App
+        props.updateUser();
+    }
+    // links that show when user is not logged in
+    let links = (
+        <span>
+            <li className="App-nav-link">
+                <Link to="/auth/login" className="App-link">Login</Link>
+            </li>
+            <li className="App-nav-link">
+                <Link to="/auth/signup" className="App-link">Sign up</Link>
+            </li>
+        </span>
+    )
+
+    // links that show when user is logged in
+    if (props.user) {
+        links = (
+          <span>
+            <li>Hello {props.user.name}!</li>
+            <li className="App-nav-link"> 
+              <Link to="/profile" className="App-link">Profile</Link>
+            </li>
+            <li className="App-nav-link">
+              <Link to="/" onClick={handleLogout} className="App-link">Log out</Link>
+            </li>
+          </span>
+        )
+    }
+
     return(
         <nav>
             <ul className="App-nav">
@@ -12,12 +45,7 @@ export default function Nav(props) {
                 <li className="App-nav-link">
                     <Link to="/authors" className="App-link">Authors</Link>
                 </li>
-                <li className="App-nav-link">
-                    <Link to="/auth/login" className="App-link">Login</Link>
-                </li>
-                <li className="App-nav-link">
-                    <Link to="/auth/signup" className="App-link">Sign up</Link>
-                </li>
+                {links}
             </ul>
         </nav>
     )
